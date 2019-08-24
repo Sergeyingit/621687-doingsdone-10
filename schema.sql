98d27f0079f61bc6d0afa7df04655043eebdc54f
sergeyingit <sergeyakkaynt@gmail.com>
2019-08-24 17:12:31 +0300

    Добавляет БД

---
+++ schema.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE doingsdone;
+
+USE doingsdone;
+
+CREATE TABLE project (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name CHAR(128) NOT NULL UNIQUE,
+  user_id INT
+);
+
+CREATE TABLE tasks (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  complete TINYINT DEFAULT 0,
+  name CHAR(128) NOT NULL,
+  file TEXT,
+  date_completed date,
+  user_id INT,
+  project_id INT
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  email CHAR(128) NOT NULL UNIQUE,
+  name CHAR(128) NOT NULL,
+  password  CHAR(64) NOT NULL
+);
