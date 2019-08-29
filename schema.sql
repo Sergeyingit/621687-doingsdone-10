62fb147ce4af4dfaac62a8a5764fc966d3fa2197
sergeyingit <sergeyakkaynt@gmail.com>
2019-08-29 09:17:52 +0300

    правки к предыдущему заданию

--- schema.sql
+++ schema.sql
@@ -13,9 +13,8 @@ CREATE TABLE tasks (
   date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   complete TINYINT DEFAULT 0,
   name CHAR(128) NOT NULL,
-  file TEXT,
+  file VARCHAR,
   date_completed date,
-  user_id INT,
   project_id INT
 );
 
@@ -24,7 +23,7 @@ CREATE TABLE users (
   date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   email CHAR(128) NOT NULL UNIQUE,
   name CHAR(128) NOT NULL,
-  password  CHAR(64) NOT NULL
+  password  CHAR(128) NOT NULL
 );
 
 CREATE INDEX project_name ON projects(name);
