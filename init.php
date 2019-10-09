680059ef52e1ee4cceca225317f1b41bbd6bf2ae
sergeyingit <sergeyakkaynt@gmail.com>
2019-10-09 22:13:07 +0300

    правки

--- init.php
+++ init.php
@@ -4,6 +4,5 @@ require_once('functions.php');
 require_once('db.php');
 session_start();
 date_default_timezone_set('Europe/Moscow');
-// error_reporting(0);
-// $navigation = [];
+
 $title = 'Дела в порядке';
